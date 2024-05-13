53e7ccb8645c0d0c1f26c5d03c87db9ca29c9723
Daniel Jarvis <45226060+JarvisSan22@users.noreply.github.com>
2024-05-13 11:37:45 +0900

    Update siteConfig.js

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -11,7 +11,7 @@
 const siteConfig = {
   title: "Precious Plastic Toolbox", // Title for your website.
   tagline: "A website for testing",
-  url: "https://JarvisSan22.github.io", // Your website URL
+  url: "https://jarvissan22.github.io", // Your website URL
   baseUrl: "/academy_JP/",
   onPageNav: 'separate',
   // remove /docs/ prefix
@@ -22,13 +22,13 @@ const siteConfig = {
 
   // Used for publishing and more
   projectName: "academy_JP",
-  organizationName: "JarvisSan22",
+  organizationName: "jarvissan22",
   // For top-level user or org sites, the organization is still the same.
   // e.g., for the https://JoelMarcey.github.io site, it would be set like...
   //   organizationName: 'JoelMarcey'
 
   // For no header links in the top nav bar -> headerLinks: [],
-  headerLinks: [{ doc: "intro", label: "Academy_JP" }],
+  headerLinks: [{ doc: "intro", label: "Academy" }],
 
 
 
